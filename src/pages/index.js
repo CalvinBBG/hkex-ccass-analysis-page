f090ef4e3964d4a76ebc1572ca8c510318efa28e
Ki Wang Wong <kiwangwong@Kide-MacBook-Pro.local>
2020-06-25 22:48:45 +0800

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+import Copyright from '../components/Copyright';
+import ContainedButtons from '../components/Button';
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Gatsby v4-beta example
+        </Typography>
+        <Link to="/about" color="secondary">
+          Go to the about page
+        </Link>
+
+
+        <ContainedButtons/>
+
+
+        <Copyright/>
+          ABCDE
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
